71eb88355c5994e690951b816107b4b372c4809f
Arnob-sen <arnobsen16.2@gmail.com>
2022-12-20 22:52:23 +0600

    updated_code

--- game_code.cpp
+++ game_code.cpp
@@ -5,56 +5,56 @@
 #include<windows.h>
 #include<stdlib.h>
 /*/
+using namespace std;
+typedef long long ll;
 
 int endfunc(int e, int a);
 
 int main(){
     //direction
-    int gd,gm,rx=200,ry=200,x[200],y[200],d;
-    for(int i = 0;i < 200 ; i++){
-        x[i]=0;
-        y[i]=0;
-    }
+    int gd,gm,food_x=200,food_y=200,snake_x[200]={0},snake_y[200]={0},d;
+
     //Food eaten
-    int f=1;
-    int chk = 700;
+    ll score=1;
+    ll  chk = 600;
     //directions
-    int dir=1;
+    ll dir=1;
+    ll snake_length=2;
     detectgraph(&gd,&gm);
     initgraph(&gd,&gm,"");
     setfillstyle(1,1);//(style,color)
-    x[0]=200,y[0]=200;
+    snake_x[0]=200,snake_y[0]=200;
     d=1;
-    int length = 1;
 
-for(;;){
+
+while(1){
     setfillstyle(1,0);
     bar(0,0,640,480);
-    setfillstyle(1,2);
+    setfillstyle(1,3);
     bar(0,0,640,10);
     bar(0,0,10,480);
     bar(0,480,640,470);
     bar(630,10,640,480);
        // food taken?
-   if(x[0]==rx && y[0]==ry ){
-    length += 1;
-    f=f+1;
+   if(snake_x[0]==food_x && snake_y[0]==food_y ){
+    snake_length ++;
+    score++;
     setfillstyle(1,0);
-    bar(rx,ry,rx+10,ry+10);
+    bar(food_x,food_y,food_x+10,food_y+10);
 
     //make food
     do{
-	rx = (1+rand()%630);
-	ry = (1+rand()%470);
-    }while(getpixel(rx,ry)!=0 && rx > 10 && ry > 10);
+	food_x = (1+rand()%630);
+	food_y = (1+rand()%470);
+    }while(getpixel(food_x,food_y)!=0 && food_x > 10 && food_y > 10);
 
-    rx=rx/10;
-    rx=rx*10;
-    ry=ry/10;
-    ry=ry*10;
+    food_x=food_x/10;
+    food_x=food_x*10;
+    food_y=food_y/10;
+    food_y=food_y*10;
     setfillstyle(1,2);
      }
-    bar(rx,ry,rx+10,ry+10);
+    bar(food_x,food_y,food_x+10,food_y+10);
     if(GetAsyncKeyState(VK_RIGHT)){d=1;}
     else if(GetAsyncKeyState(VK_LEFT)){ d=2;}
     else if(GetAsyncKeyState(VK_UP)){ d=3;}
@@ -62,50 +62,50 @@ for(;;){
     else{d=0;}
     switch(d){
     case 0:
-      if(dir==1){x[0]=x[0]+10;}
-      else if(dir==2){x[0]=x[0]-10;}
-      else if(dir==3){ y[0]=y[0]-10;}
-      else if(dir==4) {y[0]=y[0]+10;}
+      if(dir==1){snake_x[0]=snake_x[0]+10;}
+      else if(dir==2){snake_x[0]=snake_x[0]-10;}
+      else if(dir==3){ snake_y[0]=snake_y[0]-10;}
+      else if(dir==4) {snake_y[0]=snake_y[0]+10;}
       else{d=0;}
       break;
     case 1:
-           x[0]=x[0]+10;
+           snake_x[0]=snake_x[0]+10;
            dir=1;
            break;
     case 2:
-         x[0]= x[0]-10;
+         snake_x[0]-=10;
          dir=2;
           break;
     case 3:
         dir=3;
-        y[0]=y[0]-10;
+        snake_y[0]=snake_y[0]-10;
         break;
     case 4:
         dir=4;
-       y[0]= y[0]+10;
+       snake_y[0]+=10;
         break;
     }
-    for(int i = 0; i < length;i++){
-        bar(x[i],y[i],x[i]+10,y[i]+10);
+    for(ll i = 0; i <snake_length;i++){
+        bar(snake_x[i],snake_y[i],snake_x[i]+10,snake_y[i]+10);
     }
     for(int i= 199; i >0;i--){
-        x[i] = x[i-1];
-        y[i] = y[i -1];
+        snake_x[i] = snake_x[i-1];
+        snake_y[i] = snake_y[i -1];
     }
     delay(100);
-    if(x[0] >= 640 || x[0]<=0|| y[0]<=0 || y[0]>=480){
-    endfunc(f,0);
+    if(snake_x[0] >= 640 || snake_x[0]<=0|| snake_y[0]<=0 || snake_y[0]>=480){
+    endfunc(score,0);
     break;
     }
 
-    for(int i = 2; i < length;i++){
-        if(x[0] == x[i] && y[0] == y[i]){
+    for(int i = 2; i < snake_length;i++){
+        if(snake_x[0] == snake_x[i] && snake_y[0] == snake_y[i]){
             chk = i;
             break;
             }
     }
-    if(x[0] == x[chk] && y[0] == y[chk]){
-    endfunc(f,1);
+    if(snake_x[0] == snake_x[chk] && snake_y[0] == snake_y[chk]){
+    endfunc(score,1);
     break;
     }
 }
@@ -117,12 +117,12 @@ int endfunc(int e,int a){
     bar(0,0,640,470);
     system("cls");
     if(a == 0){
-        printf("You died outside the boundary!!!\n");
+        cout<<"You died outside the boundary!!!\n"<<endl;;
     }
     else if(a== 1){
-        printf("You died into yourself!!!\n");
+        cout<<"You died into yourself!!!\n"<<endl;
     }
-    printf("Your score is : %d\n", e);
+    cout<<"Your score is : "<<e<<endl;
     getch();
     return 0;
 }
